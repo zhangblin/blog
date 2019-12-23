08cacbb935f027dcedef0104fad52d4b971f3aaa
zhangbaolin <zhangblin@outlook.com>
2019-12-23 16:12:00 +0800

    转向链接

--- usr/themes/Arco-X-Kratos/index.php
+++ usr/themes/Arco-X-Kratos/index.php
@@ -17,12 +17,12 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 			<article class="kratos-hentry clearfix">
 				<div class="kratos-entry-border-new clearfix">
 					<div class="kratos-entry-thumb-new">
-						<a href="<?php isset($this->fields->redirect_url)? $this->fields->redirect_url:$this->permalink(); ?>"><img src="<?php echo showThumb($this,null,true); ?>"></a>
+						<a href="<?php empty($this->fields->redirect_url())? $this->fields->redirect_url():$this->permalink(); ?>"><img src="<?php echo showThumb($this,null,true); ?>"></a>
 					</div>
 					<div class="kratos-post-inner-new">
 						<header class="kratos-entry-header-new">
 							<a class="label"><?php $this->category(',',false); ?><i class="label-arrow"></i></a>
-							<h2 class="kratos-entry-title-new"><a href="<?php isset($this->fields->redirect_url)? $this->fields->redirect_url:$this->permalink(); ?>"><?php $this->title(); ?></a></h2>
+							<h2 class="kratos-entry-title-new"><a href="<?php empty($this->fields->redirect_url())? $this->fields->redirect_url():$this->permalink(); ?>"><?php $this->title(); ?></a></h2>
 						</header>
 						<div class="kratos-entry-content-new">
 							<p><?php $this->excerpt(90, '...'); ?></p>
@@ -31,13 +31,13 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 					<div class="kratos-post-meta-new">
 					<span class="visible-lg visible-md visible-sm pull-left">
 					<a href="#"><i class="fa fa-calendar"></i> <?php $this->date('Y/m/d'); ?></a>
-					<a href="<?php isset($this->fields->redirect_url)? $this->fields->redirect_url:$this->permalink(); ?>#respond"><i class="fa fa-commenting-o"></i> <?php $this->commentsNum('0', '1', '%d'); ?> </a>
+					<a href="<?php empty($this->fields->redirect_url())? $this->fields->redirect_url():$this->permalink(); ?>#respond"><i class="fa fa-commenting-o"></i> <?php $this->commentsNum('0', '1', '%d'); ?> </a>
 					</span>
 					<span class="pull-left">
-					<a href="<?php isset($this->fields->redirect_url)? $this->fields->redirect_url:$this->permalink(); ?>"><i class="fa fa-eye"></i> <?php get_post_view($this); ?> </a>
+					<a href="<?php empty($this->fields->redirect_url())? $this->fields->redirect_url():$this->permalink(); ?>"><i class="fa fa-eye"></i> <?php get_post_view($this); ?> </a>
 					</span>
 					<span class="pull-right">
-					<a class="read-more" href="<?php isset($this->fields->redirect_url)? $this->fields->redirect_url:$this->permalink(); ?>" title="Read More">更多 <i class="fa fa-chevron-circle-right"></i></a>
+					<a class="read-more" href="<?php empty($this->fields->redirect_url())? $this->fields->redirect_url():$this->permalink(); ?>" title="Read More">更多 <i class="fa fa-chevron-circle-right"></i></a>
 					</span>
 					</div>
 				</div>
