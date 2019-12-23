1a5a97b7dffacbca5e4e429a0f417e66e3c5b23e
zhangbaolin <zhangblin@outlook.com>
2019-12-23 15:46:43 +0800

    修改首页 链接跳转 加入转向链接

--- usr/themes/Arco-X-Kratos/index.php
+++ usr/themes/Arco-X-Kratos/index.php
@@ -8,21 +8,26 @@
  * @link https://www.vtrois.com/theme-kratos.html
  */
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
- $this->need('header.php');?>
+ $this->need('header.php'); ?>
 	<div class="container">
 		<div class="row">
 			<section id="main" class="col-md-8">
 			<?php if($this->have()):?>
-			<?php while($this->next()): ?>
+			<?php while($this->next()):
+                    if(isset($this->fields->redirect_url)){
+                        $url = $this->fields->fieldName;
+                    }else{
+                        $url =$this->permalink();
+                    }?>
 			<article class="kratos-hentry clearfix">
 				<div class="kratos-entry-border-new clearfix">
 					<div class="kratos-entry-thumb-new">
-						<a href="<?php $this->permalink(); ?>"><img src="<?php echo showThumb($this,null,true); ?>"></a>
+						<a href="<?php  echo $url; ?>"><img src="<?php echo showThumb($this,null,true); ?>"></a>
 					</div>
 					<div class="kratos-post-inner-new">
 						<header class="kratos-entry-header-new">
 							<a class="label"><?php $this->category(',',false); ?><i class="label-arrow"></i></a>
-							<h2 class="kratos-entry-title-new"><a href="<?php $this->permalink(); ?>"><?php $this->title(); ?></a></h2>
+							<h2 class="kratos-entry-title-new"><a href="<?php  echo $url; ?>"><?php $this->title(); ?></a></h2>
 						</header>
 						<div class="kratos-entry-content-new">
 							<p><?php $this->excerpt(90, '...'); ?></p>
@@ -31,13 +36,13 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 					<div class="kratos-post-meta-new">
 					<span class="visible-lg visible-md visible-sm pull-left">
 					<a href="#"><i class="fa fa-calendar"></i> <?php $this->date('Y/m/d'); ?></a>
-					<a href="<?php $this->permalink(); ?>#respond"><i class="fa fa-commenting-o"></i> <?php $this->commentsNum('0', '1', '%d'); ?> Comments</a>
+					<a href="<?php  echo $url; ?>#respond"><i class="fa fa-commenting-o"></i> <?php $this->commentsNum('0', '1', '%d'); ?> </a>
 					</span>
 					<span class="pull-left">
-					<a href="<?php $this->permalink(); ?>"><i class="fa fa-eye"></i> <?php get_post_view($this); ?> Views</a>
+					<a href="<?php  echo $url; ?>"><i class="fa fa-eye"></i> <?php get_post_view($this); ?> </a>
 					</span>
 					<span class="pull-right">
-					<a class="read-more" href="<?php $this->permalink(); ?>" title="Read More">Read More <i class="fa fa-chevron-circle-right"></i></a>
+					<a class="read-more" href="<?php  echo $url; ?>" title="Read More">更多 <i class="fa fa-chevron-circle-right"></i></a>
 					</span>
 					</div>
 				</div>
